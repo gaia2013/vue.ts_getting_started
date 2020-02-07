d897b1d63a9bddbb491ba0c72dfef6eacb8a9968
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2020-02-08 00:24:47 +0900

    first component MyButton

---
+++ src/components/MyButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button>MyButton</button>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator";
+
+  @Component
+  export default class MyButton extends Vue{
+  }
+</script>
+
